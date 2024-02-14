9a5a167beb5513565e14915e58e89814c0cb0c08
Healyr7 <healyr7@tcd.ie>
2024-02-14 00:32:40 +0000

    start of structured backend
    also added readme for running api

---
+++ backend/services.py
@@ -0,0 +1,6 @@
+# this is where we will import all the other files
+# it will also call many helper functions in those files
+
+# it will be used by main.py
+
+# there should be at least one function for each interactive API endpoint
