b2943185899672b872cecf2839991a9de4c40d8f
joe holmes <joholmes@tcd.ie>
2024-02-14 21:24:24 +0000

    updated customer get

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify # may require you to run `pip install flask` on your machine
-from database import * 
+from database import *
+from database import customerDatabase
 app = Flask(__name__)
 
 # home page of backend
@@ -7,50 +8,21 @@ app = Flask(__name__)
 def main():
   return "hello world!"
 
-customers = {
-    1 : {
-        "id" : 1,
-        "firstName" : "James",
-        "lastName" : "Humphrey",
-        "dateofBirth" : "06/07/1995",
-        "address" : "2 Oak Avenue",
-        "height" : 150,
-        "weight" : 80,
-        "smoker" : "TRUE"
-        },
-    2 : {
-        "id" : 2,
-        "firstName" : "Emma",
-        "lastName" : "Part",
-        "dateofBirth" : "04/01/1957",
-        "address" : "12 Coastal Avenue",
-        "height" : 120,
-        "weight" : 57,
-        "smoker" : "FALSE"
-        },
-    3 : {
-        "id" : 3,
-        "firstName" : "Drew",
-        "lastName" : "Kennedy",
-        "dateofBirth" : "17/03/1972",
-        "address" : "CoalView House",
-        "height" : 175,
-        "weight" : 85,
-        "smoker" : "TRUE"
-        }
-        
-    }
 
-url = "http://127.0.0.1:5000/customers"
-url1 = "http://127.0.0.1:5000/customers/1/all"
 # make similar "pages" that handle get, post, etc. requests to api.
-@app.route('/customers')
+@app.route('/customers', methods =['GET'])
 def access_Customers():
-    return jsonify(customers)
+    return jsonify(customerDatabase), 200
 
-@app.route('/customers/{id}/{itemtoAccess}')
-def access_Val(id, itemtoAccess):
-  return access_Value(id, itemtoAccess)
+@app.route('/customers/<int:id>/<string:itemtoAccess>', methods=['GET'])
+def access_Customer(id, itemtoAccess):
+  if id in customerDatabase:
+    if itemtoAccess in customerDatabase[id] or itemtoAccess.lower() == 'all' :
+      return access_Value(id, itemtoAccess), 200
+    else:
+      return {'error' : 'item not found'}, 404
+  else:
+    return {'error' : 'id not found'}, 404
   
 
                  
@@ -58,7 +30,7 @@ def access_Val(id, itemtoAccess):
 if __name__ == '__main__':
   app.run()
 
-
-requests.get(url)
-requests.get(url1)
+#test urls
+url = "http://127.0.0.1:5000/customers"
+url1 = "http://127.0.0.1:5000/customers/1/all"
 
