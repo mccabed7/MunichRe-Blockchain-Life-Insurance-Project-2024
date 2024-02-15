f0f83d9b720d462fd55868c0e147c2c8c4522b3b
joe holmes <joholmes@tcd.ie>
2024-02-15 10:53:51 +0000

    add customer post endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify # may require you to run `pip install flask` on your machine
+import requests
 from database import *
 from database import customerDatabase
 app = Flask(__name__)
@@ -7,30 +8,31 @@ app = Flask(__name__)
 @app.route("/")
 def main():
   return "hello world!"
-
+ 
 
 # make similar "pages" that handle get, post, etc. requests to api.
 @app.route('/customers', methods =['GET'])
-def access_Customers():
+def get_Customers():
     return jsonify(customerDatabase), 200
 
 @app.route('/customers/<int:id>/<string:itemtoAccess>', methods=['GET'])
-def access_Customer(id, itemtoAccess):
-  if id in customerDatabase:
-    if itemtoAccess in customerDatabase[id] or itemtoAccess.lower() == 'all' :
-      return access_Value(id, itemtoAccess), 200
+def get_Customer(id, itemtoAccess):
+    if id in customerDatabase:
+      if itemtoAccess in customerDatabase[id] or itemtoAccess.lower() == 'all' :
+        print("Accessing values")
+        return access_Value(id, itemtoAccess), 200
+      else:
+        return {'error' : 'item not found'}, 404
     else:
-      return {'error' : 'item not found'}, 404
-  else:
-    return {'error' : 'id not found'}, 404
-  
+      return {'error' : 'id not found'}, 404
+ 
+@app.route('/customers', methods =['POST'])
+def post_Customer():
+    pass
 
-                 
 
+                
 if __name__ == '__main__':
   app.run()
 
-#test urls
-url = "http://127.0.0.1:5000/customers"
-url1 = "http://127.0.0.1:5000/customers/1/all"
 
