4879e78086f0e509475176ddf05e7de5bd2cfabe
joe holmes <joholmes@tcd.ie>
2024-02-15 12:34:54 +0000

    updated customer post, needs fixing

--- backend/main.py
+++ backend/main.py
@@ -26,10 +26,20 @@ def get_Customer(id, itemtoAccess):
     else:
       return {'error' : 'id not found'}, 404
  
-@app.route('/customers', methods =['POST'])
-def post_Customer():
-    pass
-
+@app.route('/customers', methods =['POST'])     #Need to fix, "method not allowed for requested url"
+def add_newCustomer():
+   newCustomer = request.get_json()
+   firstName = newCustomer.get("firstName", "")
+   lastName = newCustomer.get("lastName", "")
+   dateofBirth = newCustomer.get("dateofBirth", "")
+   address = newCustomer.get("address", "")
+   height = newCustomer.get("height", "")
+   weight = newCustomer.get("weight", "")
+   smoker = newCustomer.get("smoker", "")
+   add_Customer(firstName, lastName, dateofBirth, address, height, weight, smoker)
+   return jsonify({len(customerDatabase) : customerDatabase[len(customerDatabase) - 1]})
+   
+      
 
                 
 if __name__ == '__main__':
