df5d3197f45001087fe738c137b3c4636b21f1b0
joe holmes <joholmes@tcd.ie>
2024-02-16 19:43:51 +0000

    Fixed customer post

--- backend/main.py
+++ backend/main.py
@@ -11,9 +11,21 @@ def main():
  
 
 # make similar "pages" that handle get, post, etc. requests to api.
-@app.route('/customers', methods =['GET'])
-def get_Customers():
-    return jsonify(customerDatabase), 200
+@app.route('/customers', methods =['GET', 'POST'])
+def customers_Request():
+    if request.method == 'GET' :
+      return jsonify(customerDatabase), 200
+    elif request.method == 'POST':
+        newCustomer = request.get_json()
+        firstName = newCustomer.get("firstName", "")
+        lastName = newCustomer.get("lastName", "")
+        dateofBirth = newCustomer.get("dateofBirth", "")
+        address = newCustomer.get("address", "")
+        height = newCustomer.get("height", "")
+        weight = newCustomer.get("weight", "")
+        smoker = newCustomer.get("smoker", "")
+        add_Customer(firstName, lastName, dateofBirth, address, height, weight, smoker)
+        return jsonify({len(customerDatabase) : customerDatabase[len(customerDatabase) - 1]})
 
 @app.route('/customers/<int:id>/<string:itemtoAccess>', methods=['GET'])
 def get_Customer(id, itemtoAccess):
@@ -26,22 +38,7 @@ def get_Customer(id, itemtoAccess):
     else:
       return {'error' : 'id not found'}, 404
  
-@app.route('/customers', methods =['POST'])     #Need to fix, "method not allowed for requested url"
-def add_newCustomer():
-   newCustomer = request.get_json()
-   firstName = newCustomer.get("firstName", "")
-   lastName = newCustomer.get("lastName", "")
-   dateofBirth = newCustomer.get("dateofBirth", "")
-   address = newCustomer.get("address", "")
-   height = newCustomer.get("height", "")
-   weight = newCustomer.get("weight", "")
-   smoker = newCustomer.get("smoker", "")
-   add_Customer(firstName, lastName, dateofBirth, address, height, weight, smoker)
-   return jsonify({len(customerDatabase) : customerDatabase[len(customerDatabase) - 1]})
-   
-      
-
-                
+                 
 if __name__ == '__main__':
   app.run()
 
