c2461cdabb7dad4384311c393711c64363796a84
joe holmes <joholmes@tcd.ie>
2024-02-19 22:24:13 +0000

    Added comments

--- backend/main.py
+++ backend/main.py
@@ -16,9 +16,9 @@ def main():
 @app.route('/customers', methods =['GET', 'POST'])        #define /customers endpoint for methods Get and Post
 def customers_Request():
     if request.method == 'GET' :                          #if request is a Get
-      return jsonify(customerDatabase), 200               #return the database in json format and status code 200(OK)
-    elif request.method == 'POST':                        #if request is a Post
-        newCustomer = request.get_json()                  #newCustomer stores values passed from request 
+      return jsonify(customerDatabase), 200               # return the database in json format and status code 200(OK)
+    elif request.method == 'POST':                        #else if request is a Post
+        newCustomer = request.get_json()                  # newCustomer stores values passed from request 
         firstName = newCustomer.get("firstName", "")      
         lastName = newCustomer.get("lastName", "")
         dateofBirth = newCustomer.get("dateofBirth", "")
@@ -32,35 +32,35 @@ def customers_Request():
 @app.route('/customers/<int:id>/<string:itemtoAccess>', methods=['GET', 'POST', 'DELETE']) #define /customers/id/itemtoAccess endpoint for methods Get, Post and Delete
 def customer_Item_Request(id, itemtoAccess):
   if id in customerDatabase:                                                               #if passed id is present in databse
-    if request.method == 'GET':                                                            #if request is a Get
-      if itemtoAccess in customerDatabase[id] or itemtoAccess.lower() == 'all' :           #if passed item is in customer with passed id or passed item is "all"
-        return access_Value(id, itemtoAccess), 200                                         #return corresponding item and status code 200(OK)
-      else:                                                                                #else return item not found error and status 404(Not Found)
+    if request.method == 'GET':                                                            #  if request is a Get
+      if itemtoAccess in customerDatabase[id] or itemtoAccess.lower() == 'all' :           #    if passed item is in customer with passed id or passed item is "all"
+        return access_Value(id, itemtoAccess), 200                                         #      return corresponding item and status code 200(OK)
+      else:                                                                                #      else return item not found error and status 404(Not Found)
         return {'error' : 'item not found'}, 404   
-    elif request.method == 'POST':                                                         #if request is a Post
-      if itemtoAccess not in customerDatabase[id]:                                         #if itemtoAccess does not already exist in specified customer   
-        newItem = request.get_json()                                                       #store passed json in newItem
-        itemtoAdd = newItem.get(itemtoAccess, "")                                          #store value of item in itemtoAdd
-        modify_Value(id, itemtoAccess, itemtoAdd)                                          #add key, value pair
-        return jsonify(customerDatabase[id]), 201                                          #return specified customer and status 201(Created)
-    elif request.method == 'DELETE':
-      if itemtoAccess in customerDatabase[id] or itemtoAccess.lower() == 'all':
-        delete_Value(id, itemtoAccess)
-        return jsonify({"":""}), 204
-      else:
+    elif request.method == 'POST':                                                         #  else if request is a Post
+      if itemtoAccess not in customerDatabase[id]:                                         #    if itemtoAccess does not already exist in specified customer   
+        newItem = request.get_json()                                                       #      store passed json in newItem
+        itemtoAdd = newItem.get(itemtoAccess, "")                                          #      store value of item in itemtoAdd
+        modify_Value(id, itemtoAccess, itemtoAdd)                                          #      add key, value pair
+        return jsonify(customerDatabase[id]), 201                                          #      return specified customer and status 201(Created)
+    elif request.method == 'DELETE':                                                       #  else if request is Delete
+      if itemtoAccess in customerDatabase[id] or itemtoAccess.lower() == 'all':            #    if itemtoAccess exists or is "all"
+        delete_Value(id, itemtoAccess)                                                     #      delete specified customer's item or all customer's data
+        return jsonify({"":""}), 204                                                       #      return error code 204(No Content)
+      else:                                                                                #    else return item not found error and status 404(Not Found)
         return jsonify({'message': 'item not found'}), 404
-  else:                                                                                                                    
+  else:                                                                                    #else return id not found error and 404(Not Found)                             
         return {'error' : 'id not found'}, 404
   
-@app.route('/customers/<int:id>/update', methods=['PATCH'])
-def update_Customer(id):
-  if id in customerDatabase:
-    updatedValues = request.get_json()
-    for key, value in updatedValues.items():
-      customerDatabase[id][key] = value
-    return jsonify(customerDatabase[id])
+@app.route('/customers/<int:id>/update', methods=['PATCH'])   #define endpoint /customers/id/update for method Patch
+def update_Customer(id): 
+  if id in customerDatabase:                                  #if id is present in database
+    updatedValues = request.get_json()                        # store json passed with the request in updatedValues
+    for key, value in updatedValues.items():                  # for each key, value pair in updatedValues
+      customerDatabase[id][key] = value                       #   assign its value to correspding key in customer database
+    return jsonify(customerDatabase[id]), 200                 # return customer's data along with status 200(OK)
   else:
-    return {'error' : 'id not found'}, 404
+    return {'error' : 'id not found'}, 404                    #else return error id not found and status 404(Not Found)
      
 
 if __name__ == '__main__':
