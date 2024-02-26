e4ec563416f836604b250479675f40912f973ecb
Declan <mccabed7@tcd.ie>
2024-02-26 15:33:27 +0000

    Added dependency: 'canvasjs/charts'

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Routes, useMatch } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import Timeline from './Timeline';
 import './Dashboard.css';
 
 
@@ -17,9 +18,9 @@ function Dashboard(){
     return(
         <div className='dashboard' style={{ display: 'flex', color: 'white' }}>
             <Sidebar items={items}  onSidebarClick={handleSidebarClick} />
-            <div style={{marginLeft: '250px', marginTop: '20px', marginRight: '20px', padding: '20px' }}>
+            <div style={{marginLeft: '20px', marginTop: '20px', marginRight: '20px', padding: '20px' }}>
                 {currentPage === 'Summary' && <div>Summaryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy</div>}
-                {currentPage === 'Risk Timeline' && <div>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>}
+                {currentPage === 'Risk Timeline' && <Timeline/>}
                 {currentPage === 'Your Premium' && <div>ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</div>}
                 {currentPage === 'Submit Info' && <div>IIIIIIIIIIIIIFJISDJF:I:JSIOEJIOSJDGOHSOIGJSIORHGOISFJ</div>}
             </div>
--- frontend/src/Timeline.js
+++ frontend/src/Timeline.js
@@ -1,11 +1,13 @@
-import '.Timeline.css';
+import './Timeline.css';
 
 //https://canvasjs.com/react-charts/line-chart/  Use this it looks good.
 
 function Timeline(){
     return(
         <div className = 'Timeline'>
-
+            This is the timeline
         </div>
     )
 }
+
+export default Timeline;
