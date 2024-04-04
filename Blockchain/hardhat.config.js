fcac48b4d3cc260b6a9fd33125c0c950f32c0daf
Sean Lawlor <lawlorse@tcd.ie>
2024-04-04 23:34:40 +0100

    added package.json , .env and hardhat config

--- Blockchain/hardhat.config.js
+++ Blockchain/hardhat.config.js
@@ -1,17 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
-  solidity: "0.7.3",
-  defaultNetwork: "sepolia",
-  networks: {
-    hardhat: {},
-    sepolia: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`]
-    }
-  },
+ solidity: "0.8.19",
+ defaultNetwork: "sepolia",
+ networks: {
+   hardhat: {},
+   sepolia: {
+     url: "https://eth-sepolia.g.alchemy.com/v2/ceHQvtvIy_lbuyKaZ-J6SxD-Wkr-7zEm",
+     accounts: ["241548e811c77fa4160d7916062a53fa680b349725591ebfe54d946d721dacc4"]
+   }
+ },
 }
